4507f7fed29f0444925984df5d82d16dcdf68162
marxearangco <arangco.marco@gmail.com>
2015-07-09 09:55:25 +0800

    PIS initiated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PIS::Application.config.secret_key_base = '9ff7557f1a495aabcd0e1e8a4c7bacd6f0ef0caacb66c3ad53b260aea4802fc4c4bbb601f01dec754a32642db3eb87648267a962ba2be17118186cec5416e9e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PIS::Application.config.session_store :cookie_store, key: '_PIS_session'
