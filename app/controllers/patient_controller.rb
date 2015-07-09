022351069bd594e059a430cd4079bb166e82ed12
marxearangco <arangco.marco@gmail.com>
2015-07-09 11:27:58 +0800

    creating form

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -2,4 +2,39 @@ class PatientController < ApplicationController
  def index
  	
  end
+
+ def show
+ end
+
+ def new
+ 	@patient = Patient.new
+ end
+
+ def create
+ 	@patient = Patient.create(patient_params)
+ 	if @patient.save
+ 		redirect_to @patient
+ 	else
+ 		render 'new'
+ 	end
+ end
+
+ def edit
+ end
+
+ def update
+ end
+
+ def destroy
+ end
+
+ private
+
+ def find_patient
+ end
+
+ def patient_params
+ 	params.require(:patient).permit(:lname, :fname, :mi, :address)
+ end
+
 end
